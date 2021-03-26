6a5417af47164cd54c2bd7c093cd7bcd8eea5553
Educani3@gmail.com <Educani3@gmail.com>
2021-03-26 12:46:56 +0100

    Prueba EC

--- app/src/main/java/com/example/proyecto_cine/MainActivity.java
+++ app/src/main/java/com/example/proyecto_cine/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //jikhsfdgjkhsdgfjksgfkjgsakfjhg
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
