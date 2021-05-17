6d0c6b5c619209d5e180a982ffa644cb12e55e0a
Natalia <nataliasanchezdel00@gmail.com>
2021-05-17 20:43:59 +0200

    Activity entrada diseño

---
+++ app/src/main/java/com/example/proyecto_cine/Cines/ActivityEntrada.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_cine.Cines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto_cine.R;
+
+public class ActivityEntrada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada);
+    }
+}
